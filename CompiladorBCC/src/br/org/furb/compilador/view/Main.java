928c635cb2643c9c3bb5bd6600de6110012699a7
maicon.gerardi <maicon.gerardi@gmail.com>
2014-06-21 23:48:37 +0000

    adicionando g0yzices

--- CompiladorBCC/src/br/org/furb/compilador/view/Main.java
+++ CompiladorBCC/src/br/org/furb/compilador/view/Main.java
@@ -1,8 +1,23 @@
 package br.org.furb.compilador.view;
 
+import java.awt.Insets;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import ch.randelshofer.quaqua.QuaquaManager;
+
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+		//SETANDO CONFIGURAÇÕES NO LOOK AND FEEL
+		System.setProperty("Quaqua.tabLayoutPolicy","wrap");
+		UIManager.put("Panel.opaque", Boolean.TRUE);
+		UIManager.put("Quaqua.Component.cellRendererFor", Boolean.TRUE);
+		UIManager.put("Component.visualMargin", new Insets(2,2,2,2));
+		//SETANDO O LOOK AND FEEL
+		UIManager.setLookAndFeel(QuaquaManager.getLookAndFeel());
+		
 		new PrincipalView();
 	}
 }
