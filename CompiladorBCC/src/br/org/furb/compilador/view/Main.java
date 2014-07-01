0878f1f87ad25062ad245ab841d915c68e4e2ee3
maicon.gerardi <maicon.gerardi@gmail.com>
2014-07-01 22:19:23 +0000

    motherfuck lookandfeel

--- CompiladorBCC/src/br/org/furb/compilador/view/Main.java
+++ CompiladorBCC/src/br/org/furb/compilador/view/Main.java
@@ -1,25 +1,21 @@
 package br.org.furb.compilador.view;
 
-import java.awt.Insets;
-
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import ch.randelshofer.quaqua.QuaquaManager;
-
 
 public class Main {
-	public static void main(String[] args) throws UnsupportedLookAndFeelException {
+	public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 		//SETANDO CONFIGURAÇÕES NO LOOK AND FEEL
-		System.setProperty("Quaqua.tabLayoutPolicy","wrap");
-		UIManager.put("Panel.opaque", Boolean.TRUE);
-		UIManager.put("Quaqua.Component.cellRendererFor", Boolean.TRUE);
-		UIManager.put("Component.visualMargin", new Insets(2,2,2,2));
-		//SETANDO O LOOK AND FEEL
-		UIManager.setLookAndFeel(QuaquaManager.getLookAndFeel());
+//		System.setProperty("Quaqua.tabLayoutPolicy","wrap");
+//		UIManager.put("Panel.opaque", Boolean.TRUE);
+//		UIManager.put("Quaqua.Component.cellRendererFor", Boolean.TRUE);
+//		UIManager.put("Component.visualMargin", new Insets(2,2,2,2));
+//		//SETANDO O LOOK AND FEEL
+//		UIManager.setLookAndFeel(QuaquaManager.getLookAndFeel());
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		
 		new PrincipalView();
 	}
 }
